61472f0cc7e161617a228f8db96cdf3726bcc093
Robin Joshua Del Mundo <robin.delmundo@slidegenius.com>
2021-05-01 00:17:47 +0800

    Lazy Touch

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function addListenerMulti(element, eventNames, listener) {
 
 }
 
-addListenerMulti(window, 'click keydown', lazyLoad);
+addListenerMulti(window, 'click keydown touchstart touchmove', lazyLoad);
 
 // Run after the HTML document has finished loading
 function lazyLoad(){
